74b7b5c8ec018755f37a81672feb6374e8f6dd7c
Heather Everton <heather.everton@planplusonline.com>
2020-08-16 17:09:56 -0600

    password generator with JS, and readme files updated.

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# PW-Gen-JS
-Password Generator in Javascript
+-->
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+
+
+<!-- PROJECT INTRO -->
+<br />
+<p align="center">
+  <a href="https://github.com/heather-everton/PW-Gen-JS">
+    <img src="./assets/images/logo.jpg" alt="Logo" width="100%" height="100%">
+  </a>
+
+  <h3 align="center">Password Generator</h3>
+
+  <p align="center">
+    An awesome password generator to help you secure your information!
+    <br />
+    <a href="https://github.com/heather-everton/PW-Gen-JS"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/othneildrew/Best-README-Template">View Demo</a>
+    ·
+    <a href="https://github.com/heather-everton/PW-Gen-JS/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/heather-everton/PW-Gen-JS/issues">Request Feature</a>
+  </p>
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+  * [Built With](#built-with)
+* [Getting Started](#getting-started)
+* [Usage](#usage)
+* [Roadmap](#roadmap)
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+<a href="https://www.github.com/heather-everton/PW-GGen-JS" rel="nofollow" >
+    <img src="./assets/images/product-screenshot.jpg" alt="Password Generator Screenshot" width="100%" height="100%">
+</a>
+
+We all have to create passwords every day for various types of data and on top of that reset our passwords for the many applications we're running. Each application has different criteria for the type of passwords they need. Now you can generate a custom password each time for all your security needs. 
+
+Here's why:
+* Your time should be focused being productive not trying to come up with uniique passwords all the time.
+* You shouldn't be using the same password for everything you access. 
+* You should be able to pick exactly what type of password you need each time. 
+
+A list of commonly used resources that I find helpful are listed in the acknowledgements.
+
+### Built With
+* [JavaScript](https://javascript.com)
+* [html](https://html.com)
+* [css](https://free-css.com)
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get started simply click on "Generate Password" and select the options for your perfect password.
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+You can get long passwords for your most sensitive data. 
+<img src="./assets/images/secure.jpg" alt="Screenshot of a very secure password." width="100%" height="100%">
+
+
+Or simple passwords for other applications. 
+<img src="./assets/images/simple.jpg" alt="Screenshot of a simple password." width="100%" height="100%">
+
+
+You can choose passwords with special characters and numbers.
+<img src="./assets/images/symbols-numbers.jpg" alt="Screenshot of password with special characters & numbers." width="100%" height="100%">
+
+Or ones with only letters. 
+<img src="./assets/images/letters.jpg" alt="Screenshot of password with letters only." width="100%" height="100%">
+
+
+<!-- ROADMAP -->
+## Roadmap
+
+See the [open issues](https://github.com/heather-everton/PW-Gen-JS/issues) for a list of proposed features (and known issues).
---
+++ assets/script.js
@@ -0,0 +1,161 @@
+//Array of uppercase 
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//Array of lowercase
+var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Array of special characters
+var specialArray = ["!", "#", "%", "&", "(", ")", "[", "]", "{", "}", ".", ";", ":", "*", "-", "+", "/", "=", "\\", "?","'","~","_"];
+
+//Array of numbers
+var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+//Array we will put avaialbe characters for our password 
+var optionArray = [];
+
+//Array we will pull a in 1 requiried character for each type
+var reqArray = [];
+
+//Array we will put our password result in 
+var resultArray = [];
+
+function pwLength(){
+  //clear arrays 
+  optionArray = [];
+  reqArray = [];
+  resultArray = [];
+
+  //ask the user what the lenght of the passwoord is supposed to be.
+  var charLength = window.prompt ("How many characters would you like your password to be?");
+  charLength = parseInt(charLength);
+  console.log ("Your password will be " + charLength + " characters long.");
+  //validate that the user chose an optioin wiithin the 8 and 128.
+  if (charLength < 8 || charLength > 128){
+    window.alert("Please choose an option between 8 and 128.");
+    pwLength();
+  } 
+  else {
+      //prompt next function and pass the length from teh window.prompt forward. 
+      return setCriteria(charLength)
+  }
+}
+
+function setCriteria(charLength){
+
+  //ask user what characters to include in PW.
+  var upperAllow = window.confirm ("Would you like to use Uppercase letters in your password?");
+    console.log ("Use Uppercase " +  upperAllow);
+  
+  var lowerAllow = window.confirm ("Would you like to use lowercase letters in your password?");
+  console.log ("Use Lowercase " + lowerAllow);
+  
+  var specialAllow = window.confirm("would you like to use special characters in your password?");
+  console.log ("Use special characters " + specialAllow);
+  
+  var numAllow = window.confirm ("Would you like to use numbers in your password?");
+  console.log ("Use numbers " + numAllow);
+
+  //make sure the user chooses at least one type of character
+  if(upperAllow === false && lowerAllow === false && specialAllow === false && numAllow === false){
+    window.alert ("Please choose at least one character type.");
+    //if the user does not choose an option kick them out and force them to start over.
+    return ;
+  }
+  else{
+    //build an object with the data from my window.confirm questions
+    var userChoices = {
+      charLength: charLength,
+      upperAllow: upperAllow,
+      lowerAllow: lowerAllow,
+      specialAllow: specialAllow,
+      numAllow: numAllow
+    };
+
+    console.log (userChoices) 
+    //prompt next function to run and pass user choices to the next fuction
+    return generatePassword(userChoices)
+  }
+}
+function generatePassword(userChoices) {
+
+  //pick one chracter at random from each array.
+  var upperReq = Math.floor(Math.random()*upperArray.length);
+  var lowerReq = Math.floor(Math.random()*lowerArray.length);
+  var sepcialReq = Math.floor(Math.random()*specialArray.length);
+  var numReq = Math.floor(Math.random()*numArray.length);
+
+  //build the optionaArray based on the answers from the window.confirm
+  if (userChoices.upperAllow === true) {
+    optionArray.push(upperArray);
+  }
+  if (userChoices.lowerAllow === true) {
+    optionArray.push(lowerArray);
+  }
+  if (userChoices.specialAllow === true) {
+    optionArray.push(specialArray);
+  }
+  if (userChoices.numAllow  === true) {
+    optionArray.push(numArray);
+  }
+  console.log(optionArray);
+
+  //put one option in the reqArray for each of the character types the user selects.
+  if (userChoices.upperAllow === true) {
+    reqArray.push(upperArray [upperReq]);
+  }
+  if (userChoices.lowerAllow === true) {
+    reqArray.push(lowerArray [lowerReq]);
+  }
+  if (userChoices.specialAllow === true) {
+    reqArray.push(specialArray [sepcialReq]);
+  }
+  if (userChoices.numAllow  === true) {
+    reqArray.push(numArray[numReq]);
+  }
+  console.log (reqArray)
+
+  //prompt th enext function ans pass the user choices forward.
+  return setPassword(userChoices)
+}
+
+
+//Create Function
+function setPassword(userChoices) {
+  //for loop uuused to generate PW
+  for (var i=0; i < userChoices.charLength; i++){
+    var firstIndex = Math.floor(Math.random()*optionArray.length);
+    //console.log (firstIndex);
+    var secondIndex = Math.floor(Math.random()*optionArray[firstIndex].length);
+    // console.log (secondIndex);
+    //console.log(optionArray[firstIndex][secondIndex])
+    resultArray.push(optionArray[firstIndex][secondIndex])
+  }
+  // console.log(resultArray)
+
+  //incluude at least one of each guaranteed characters
+    for (var i = 0; i < reqArray.length; i++){
+      resultArray[i] = reqArray[i];
+      // console.log (resultArray[i], reqArray[i]);
+  }
+  //return as a string
+  return resultArray.join('');  
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = pwLength();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+  document.passwordText('password').innerHTML = "";  
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//execute Functioin
+//generatePassword() 
